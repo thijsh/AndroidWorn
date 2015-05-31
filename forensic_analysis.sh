5cf1ae7d83ce9a054afbcfe332bda9252b962638
thijsh <git@th-d.nl>
2015-06-01 00:38:19 +0200

    Small uncommited change

--- forensic_analysis.sh
+++ forensic_analysis.sh
@@ -1,25 +1,29 @@
 #! /bin/bash
 
-# Generate bodyfile timeline of the userdata partition
+# Generate bodyfile timeline of the userdata, cache and system partition
+echo "Generating timeline bodyfiles..."
 fls -r -m "/sdcard/" -o `grep userdata partitions_img.txt | awk '{print $2}'` flash.img > userdata_timeline.txt
 mactime -b userdata_timeline.txt -d > userdata_timeline.csv
-
-# Generate bodyfile timeline of the cache partition
 fls -r -m "/cache/" -o `grep cache partitions_img.txt | awk '{print $2}'` flash.img > cache_timeline.txt
 mactime -b cache_timeline.txt -d > cache_timeline.csv
-
-# Generate bodyfile timeline of the system partition
 fls -r -m "/system/" -o `grep system partitions_img.txt | awk '{print $2}'` flash.img > system_timeline.txt
 mactime -b system_timeline.txt -d > system_timeline.csv
 
 # Run Photorec, move all files to one folder, and generate hashes
-mkdir photorec
+echo "Photorec file carving..."
+mkdir -p photorec
 photorec /debug /log /d photorec /cmd flash.img partition_none,options,mode_ext2,fileopt,everything,enable,search
 mv -f photorec.*/* photorec/
 rmdir photorec.*/
 for file in photorec/*; do sha256sum "$file"; done | sort > photorec.sha256
 
+# Run Scalpel (NOTE: disabled by default because 10GiB of files is 'recovered' from a 4GiB image)
+# echo "Scalpel file carving..."
+# mkdir -p Scalpel
+# scalpel -o scalpel/ flash.img
+
 # Find all strings and perform rough filter for notifications
+echo "String search..."
 strings -n 4 flash.img > strings.txt
 # egrep "<p dir=\"ltr\">(.{1,1000})<\/p>" strings.txt > notifications.txt
 # grep -Pzo "<p dir=\"ltr\">.*</p>" strings.txt > notifications.txt
